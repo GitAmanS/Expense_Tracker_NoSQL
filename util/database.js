39f32ea41cee678bfe20831039b6fe467afbc801
Aman Shaikh <amanshaikh8624@gmail.com>
2024-01-24 00:55:06 -0800

    project complete

---
+++ util/database.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.82wj9wt.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+    let _db;
+    const MongoConnect =  async () => {
+      try {
+        const Client =await client.connect();
+        _db = Client.db("Expensetracker");
+        console.log("successfully connected to MongoDB!");
+      } catch(error){
+        console.error(error);
+      }
+    }
+    const getDb = ()=>{
+      if(_db){
+        return _db;
+      }else{
+        throw "No Database Connection";
+      }
+    }
+   
+    exports.MongoConnect = MongoConnect;
+    exports.getDb = getDb;
